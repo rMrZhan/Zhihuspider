27cf1bd097d62bf1bd0d5ef9470db218e43c56da
Mr.zhl <895902052@qq.com>
2018-11-23 16:44:48 +0800

    1.0

--- src/main/java/com/zhihuspider/zhihuspider/HttpClient.java
+++ src/main/java/com/zhihuspider/zhihuspider/HttpClient.java
@@ -1,5 +1,7 @@
 package com.zhihuspider.zhihuspider;
 
-public class HttpClient {
+import org.apache.http.impl.client.CloseableHttpClient;
 
+public class HttpClient {
+    CloseableHttpClient httpClient;
 }
